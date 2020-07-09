ff538e8903e89406e3f113e4f4ade38d8e320897
Jelle Hak <jellehak@gmail.com>
2020-07-09 09:49:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
-var mongoose = require('mongoose');
-var fs = require('fs');
-var path = require('path');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
 
-
-var models = {}
-module.exports = {
-	connect: connect,
-	loadAllFrom: loadAllFrom
-}
-
-
-
-
-function loadAllFrom(dir) {
+const loadSchemasFrom = (dir = '') => {
 	//Import all schemas from directory
-	fs
+	const schemas = fs
 		.readdirSync(dir)
 		.filter(function (file) { return (file.indexOf(".") !== 0) && (file !== "index.js"); })
 		.map(function (file) { return path.join(dir, file); })
 		.filter(function (file) { return fs.statSync(file).isFile(); })
 		.forEach(function (file) {
 			var ret = require(file)
+			return {name: file, model: ret}
 		});
+	
+	// Register as mongoose.model
+	schemas.map(({name, model}) => {
+		mongoose.model(name, model)
+	}
 }
 
-
-
-//------------------
-//Set mongoose promise style
-// Returns promise
-//------------------
-async function connect(database) {
+const connect = async (database = '') => {
 	mongoose.Promise = global.Promise
 
 	// If the Node process ends, close the Mongoose connection 
@@ -41,10 +31,12 @@ async function connect(database) {
 		});
 	});
 
-
 	return mongoose.connect(database, {
 		useMongoClient: true,
 	})
 }
 
-
+module.exports = {
+	connect,
+	loadAllFrom
+}
